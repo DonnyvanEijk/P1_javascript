69eca36ef204ccb62bd6706c90e072b74c78444e
DonnyGames <donnyvaneijk873@gmail.com>
2022-09-15 13:54:25 +0200

    3.2 af

---
+++ 3.2/app.js
@@ -0,0 +1,11 @@
+var myAge = "15"
+var text = "Hallo?"
+var vrouw = "que?"
+var ja = "JAJA"
+var num1 = "8"
+var num2 = "8"
+var num3 = "8"
+if (myAge < 18) document.write(text + "<br>")
+
+if (text == "Hallo?") document.write(vrouw + "<br>")
+if (num1 && num2 == num3) document.write(ja)
