25133e1b00b066577cc0a8fc2a3dd54f10d01034
DonnyGames <donnyvaneijk873@gmail.com>
2022-09-15 13:32:44 +0200

    ;

--- 3.1/app.js
+++ 3.1/app.js
@@ -1,26 +1,26 @@
-const myName = "Donny"
-var myAge = "15"
-const diploma = "true"
-var passed = "undefined"
+const myName = "Donny";
+var myAge = "15";
+const diploma = "true";
+var passed = "undefined";
 
-document.write(myName + "<br>")
-document.write(myAge + "<br>")
-document.write(diploma + "<br>")
-document.write(passed + "<br>")
+document.write(myName + "<br>");
+document.write(myAge + "<br>");
+document.write(diploma + "<br>");
+document.write(passed + "<br>");
 
 //////////////////////////////////////////////////////////////////////////////////
 
-var num1 = 69
-var num2 = 420
-var answer = num1 * num2
-var answer1 = num1 - num2
-var answer2 = num1 / num2
-var answer3 = num1 + num2
-var answer4 = num1 > num2 
+var num1 = 69;
+var num2 = 420;
+var answer = num1 * num2;
+var answer1 = num1 - num2;
+var answer2 = num1 / num2;
+var answer3 = num1 + num2;
+var answer4 = num1 > num2 ;
 /////////////////////////////////////////'////////////////////////////////////////////////////
 
-document.write(answer + "<br>") 
-document.write(answer1 + "<br>")
-document.write(answer2 + "<br>") 
-document.write(answer3 + "<br>") 
-document.write(answer4 + "<br>") 
+document.write(answer + "<br>");
+document.write(answer1 + "<br>");
+document.write(answer2 + "<br>");
+document.write(answer3 + "<br>");
+document.write(answer4 + "<br>");
