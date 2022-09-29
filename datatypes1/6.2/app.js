8d79e1c166a92f0b7a16517fd11ffd0016c825ef
DonnyGames <donnyvaneijk873@gmail.com>
2022-09-29 13:45:37 +0200

    yesyeys

---
+++ datatypes1/6.2/app.js
@@ -0,0 +1,8 @@
+function kleur_aanpassen() {
+    var aardappel = document.getElementById("aardappel");
+    aardappel.style.color = "red";
+    aardappel.innerHTML = "Inhoud aangepast!";
+}
+
+
+   
