9ae590b43afb5cd451c36d266bd0ea54a0de5bea
DonnyGames <donnyvaneijk873@gmail.com>
2022-10-04 23:00:50 +0200

    6.4 lol

---
+++ datatypes1/6.4/app.js
@@ -0,0 +1,22 @@
+function calculate(x, y){
+    console.log(x + y)
+}
+calculate(15, 4)
+
+function multiplyByFive(z){
+    console.log(z * 5)
+}
+multiplyByFive(5)
+
+
+function checkInput(x, y) {
+var antwoord = Math.max(x, y)
+console.log(antwoord)
+}
+checkInput(15, 30)
+
+
+function calculateMinutes(x) {
+    console.log(x / 60)
+}
+calculateMinutes(120)
