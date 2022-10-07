09e85344850542f72b03f8ac71592319922bf857
DonnyGames <donnyvaneijk873@gmail.com>
2022-10-07 09:52:25 +0200

    yes

--- monkey-animation/app.js
+++ monkey-animation/app.js
@@ -1,14 +1,17 @@
 const closedFace = document.querySelector(".closed");
 const openFace = document.querySelector(".open");
+let text = document.getElementById("verassing");
 
 
 
 closedFace.addEventListener("click",function() {
     closedFace.style.display = "none";
     openFace.style.display = "block";
+    text.innerText = "boo";
 } )
 
 openFace.addEventListener("click",function() {
     openFace.style.display = "none";
     closedFace.style.display = "block";
+    text.innerText = "peek a";
 } )
