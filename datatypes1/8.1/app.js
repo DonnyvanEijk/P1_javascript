6df52ae99ec36baa1ab67fc2e2f3ebec1f757760
DonnyGames <donnyvaneijk873@gmail.com>
2022-10-13 12:47:55 +0200

    f

--- datatypes1/8.1/app.js
+++ datatypes1/8.1/app.js
@@ -7,6 +7,8 @@ let input2 = document.getElementById("input2");
 let x = Number(input1.value);
 let y = Number(input2.value);
 output.innerText = x + y;
+
+if (x < 0 || y < 0) output.innerText = "te laag"
 }
 function minusCalculate() {
     let input1 = document.getElementById("input1");
